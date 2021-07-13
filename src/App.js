fc0d494f99c825fe92cf72af2ce9cfc1361a64a4
sagnolkamdem <sagnolkamdem721@gmail.com>
2021-07-13 10:25:32 +0100

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-2/3 m-auto">
+      <h1 className="text-9xl">Bonjour!</h1>
+      <p className="shadow-md p-4">srdtfyhjkkchf cyuiuonk ;yij l uok</p>
+    </div>
+  );
+}
+
+export default App;
